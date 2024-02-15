59de5cf04a52468409f8616dd699256c85f7a691
De Oliveira Prescillia <oliveiraprescillia04@gmail.com>
2024-02-15 17:47:17 +0100

    supression des guillemets pour avoir acces au elements du tableau et ajout dune const pour les textes et images

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image:"./assets/images/slideshow/slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>",
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"./assets/images/slideshow/slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"./assets/images/slideshow/slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"./assets/images/slideshow/slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>",
 	}
 ]
 
 const Flechegauche = document.getElementById("left")
 const Flechedoite = document.getElementById("right")
+const TexteSlide = document.getElementById("photo")
+const ImageSlide = document.getElementById("texte")
+
+let valeur = 0
 
 Flechedoite.addEventListener('click',()=>{
 
@@ -26,5 +30,5 @@ Flechedoite.addEventListener('click',()=>{
 
 
 Flechegauche.addEventListener('click',()=>{
-	
+
 })
