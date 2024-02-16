c0b59f07172c037e028787e00058c8b00406fd9e
De Oliveira Prescillia <oliveiraprescillia04@gmail.com>
2024-02-16 19:01:59 +0100

    creation dun forEach pour ajouter les bullets sur chaque enfant du tableau slides ainsi que recuperation de class=dots + ajout de class=dot_selected sur bullets

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,40 @@
 const slides = [
-	{
-		image:"./assets/images/slideshow/slide1.jpg",
-		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>",
-	},
-	{
-		image:"./assets/images/slideshow/slide2.jpg",
-		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-	},
-	{
-		image:"./assets/images/slideshow/slide3.jpg",
-		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	},
-	{
-		image:"./assets/images/slideshow/slide4.png",
-		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>",
-	}
-]
+  {
+    image: "./assets/images/slideshow/slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
-const Flechegauche = document.getElementById("left")
-const Flechedoite = document.getElementById("right")
-const TexteSlide = document.getElementById("photo")
-const ImageSlide = document.getElementById("texte")
+const Flechegauche = document.getElementById("left");
+const Flechedoite = document.getElementById("right");
+const TexteSlide = document.getElementById("photo");
+const ImageSlide = document.getElementById("texte");
+const PositionBoxDots = document.querySelector(".dots");
 
-let valeur = 0
+let valeur = 0;
 
-Flechedoite.addEventListener('click',()=>{
+slides.forEach(() => {
+  let bullet = document.createElement("div");
+  bullet.classList.add("dot");
+  PositionBoxDots.appendChild(bullet);
+});
 
-})
+const StyleBullets = document.querySelectorAll(".dot");
+StyleBullets[valeur].classList.add("dot_selected");
 
+Flechedoite.addEventListener("click", () => {});
 
-Flechegauche.addEventListener('click',()=>{
-
-})
+Flechegauche.addEventListener("click", () => {});
